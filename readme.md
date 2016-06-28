38d70dc48b4ac0877517a62ba42c31c922bf3ed7
Richard Warg <rjwarg@gmail.com>
2016-06-28 17:09:04 +0000

    working on mongodb

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # mean_book
 
 Tutorial using mean book by Haviv
+
+Step 4: Using Mongodb and mongoose
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-var express = require('./config/express');
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+var mongoose = require('./config/mongoose'),
+express = require('./config/express');
+
+var db= mongoose();
 var app = express();
 var PORT = process.env.PORT ;
 var IP = process.env.IP;
