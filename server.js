75e8580b1d02f0f52bcc841714f0c2a4deabb391
Richard Warg <rjwarg@gmail.com>
2016-07-06 14:23:48 +0000

    stable step-4 prior to adding "website" to model

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ var db= mongoose();
 var app = express();
 var PORT = process.env.PORT ;
 var IP = process.env.IP;
-app.listen(process.env.PORT, process.env.IP);
+app.listen(PORT, IP);
 module.exports = app;
 console.log('server listening on port ' + PORT + ', IP ' + IP);
